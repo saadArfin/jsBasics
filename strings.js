5cb9972a56f0d7a232b90fddde96417aad266aaf
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-02-08 21:22:46 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+ const gameName = new String("saadarfin")
+
+ console.log(gameName[0])
+ console.log(gameName.__proto__)
+ console.log(gameName.length)
+ console.log(gameName.toUpperCase())
+ console.log(gameName.charAt(2))
+
+ //NOTE: methods like touppercase wont change the original string but will return a new string with the changes 
+ //because string is a primitive data type and is immutable
+
+ //NOTE: strings are immutable in JavaScript
