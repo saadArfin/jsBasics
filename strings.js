cdb88546ca7942b1027671e3b6d552e18b663c51
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-02-10 04:06:28 +0530

    added some comments

--- strings.js
+++ strings.js
@@ -1,12 +1,16 @@
- const gameName = new String("saadarfin")
+const gameName = new String("saadarfin");
 
- console.log(gameName[0])
- console.log(gameName.__proto__)
- console.log(gameName.length)
- console.log(gameName.toUpperCase())
- console.log(gameName.charAt(2))
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
 
- //NOTE: methods like touppercase wont change the original string but will return a new string with the changes 
- //because string is a primitive data type and is immutable
+//NOTE: methods like touppercase wont change the original string but will return a new string with the changes
+//because string is a primitive data type and is immutable
 
- //NOTE: strings are immutable in JavaScript
+//NOTE: strings are immutable in JavaScript
+
+//NOTE: AN IMPORTANT FORMULA -> Math.floor(Math.random() * (max - min + 1)) + min
+
+//NOTE: Date is an object in JavaScript
