0e1a0952c80e3ff1678955a5cd5aa83159e0dfc0
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-02-26 21:17:31 +0530

    add bind.html for button click handling and mathpi.js for property descriptor examples

---
+++ test.js
@@ -0,0 +1,14 @@
+class test{
+    constructor(){
+        this.name = "saad"
+        this.age = 20
+    }
+
+    habit(){
+        console.log("football")
+    }
+}
+
+let user = new test()
+console.log(user)
+console.log(user.__proto__);
